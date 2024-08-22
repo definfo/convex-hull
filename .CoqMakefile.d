05973c5c4d36393801c177f93881beaeced051d3
definfo <hjsdbb1@gmail.com>
2024-08-22 12:38:57 +0800

    refactor

--- .CoqMakefile.d
+++ .CoqMakefile.d
@@ -1,6 +1,6 @@
 theories/ZPoints.vo theories/ZPoints.glob theories/ZPoints.v.beautified theories/ZPoints.required_vo: theories/ZPoints.v 
 theories/ZPoints.vio: theories/ZPoints.v 
 theories/ZPoints.vos theories/ZPoints.vok theories/ZPoints.required_vos: theories/ZPoints.v 
-theories/Jarvis_March.vo theories/Jarvis_March.glob theories/Jarvis_March.v.beautified theories/Jarvis_March.required_vo: theories/Jarvis_March.v theories/ZPoints.vo
-theories/Jarvis_March.vio: theories/Jarvis_March.v theories/ZPoints.vio
-theories/Jarvis_March.vos theories/Jarvis_March.vok theories/Jarvis_March.required_vos: theories/Jarvis_March.v theories/ZPoints.vos
+theories/Graham_Scan.vo theories/Graham_Scan.glob theories/Graham_Scan.v.beautified theories/Graham_Scan.required_vo: theories/Graham_Scan.v theories/ZPoints.vo
+theories/Graham_Scan.vio: theories/Graham_Scan.v theories/ZPoints.vio
+theories/Graham_Scan.vos theories/Graham_Scan.vok theories/Graham_Scan.required_vos: theories/Graham_Scan.v theories/ZPoints.vos
--- theories/Graham_Scan.v
+++ theories/Graham_Scan.v
@@ -96,7 +96,7 @@ Fixpoint succ (p : point) (T : list point) : list point :=
   | _ => cons p T
   end.
 
-Fixpoint convex_hull_assist (P T : list point) : list point :=
+(* Fixpoint convex_hull_assist (P T : list point) : list point :=
   match P with
   (* call `succ` for each point *)
   | cons p P' => convex_hull_assist P' (succ p T)
@@ -108,56 +108,60 @@ Definition convex_hull (P : list point) : list point :=
   match P with
   | cons p P' => convex_hull_assist P' [p]
   | _ => P
+  end. *)
+
+(* Stack (T): [q ; ... ; p] *)
+Fixpoint convex_hull' (T : list point) : list point :=
+  match T with
+  | cons q T' => succ q (convex_hull' T')
+  | _ => T
   end.
 
-
-
-Fixpoint _sort (p : point) (P : list point) : Prop :=
-  match P with
-  | cons q P' =>
-    match P' with
-    | cons r _ => ccw p r q /\ _sort p P'
-    | nil => True
-    end
-  | nil => False
-  end.
-
-(* all point in P is left to p->q *)
-Fixpoint leftward (p q : point) (P : list point) : Prop :=
-  match P with
-  | cons r P' => ccw p q r /\ leftward p q P'
-  | nil => True
-  end.
-
-(* Set (P) : [ q ; r ; ... ] *)
-Fixpoint sort (p : point) (P : list point) : Prop :=
-  match P with
-  | cons q P' => leftward p q P' /\ sort p P'
-  | nil => True
-  end.
-
-(* all point in P is right to p->q *)
+(* all point in P are right to p->q *)
 Fixpoint rightward (p q : point) (P : list point) : Prop :=
   match P with
   | cons r P' => ccw p r q /\ rightward p q P'
   | nil => True
   end.
 
-(* ? for recursion *)
-Fixpoint sort' (p : point) (P : list point) : Prop :=
+(* Stack (T) : [s ; r ; q ; ... ; p] *)
+Fixpoint sort (p : point) (P : list point) : Prop :=
   match P with
-  | cons q P' => rightward p q P' /\ sort' p P'
+  | cons q P' => rightward p q P' /\ sort p P'
   | nil => True
   end.
 
+(* ? *)
+(* Definition sort' (P : list point) : Prop :=
+  match P with
+  | cons p P' => sort p P'
+  | nil => True
+  end. *)
+
+Lemma sort_rec : forall (p q : point) (P : list point),
+  sort p (q :: P) -> sort p P.
+Proof.
+  simpl; intros. destruct H. assumption. Qed.
+
 (* Stack (T) : [ s ; r ; q ; ... ; p ], check recursively from s. *)
 (* ccw q r s /\ ccw r s p *)
-(* p = last T *)
+(* p := last T *)
 Fixpoint convex (p : point) (T : list point) : Prop :=
   match T with
   | cons s (cons r (cons q T')) => ccw q r s /\ ccw r s p /\ convex p T'
   | _ => True
   end.
+
+(* ? *)
+(* Definition convex' (T : list point) : Prop :=
+  match rev T with
+  | cons p _ => convex p T
+  | nil => True
+  end. *)
+
+(** Verification *)
+Definition point_In_dec := In_dec point_dec.
+Check point_In_dec.
 
 Theorem succ_in : forall (p : point) (T : list point),
   In p (succ p T).
@@ -171,23 +175,73 @@ Proof.
       * auto.
 Qed.
 
-(* After ___,
-the vertices on T are the vertices of C2
+(*  After init,
+the vertices on T are the vertices of C_2
 in clockwise order. *)
 Theorem convex_0 : forall (p q r : point),
-  sort' p [r ; q ; p] -> convex p [q ; p] ->
+  sort p [r ; q ; p] -> convex p [q ; p] ->
   convex p [r ; q ; p].
 Proof.
   simpl; intros.
   destruct H. destruct H.
-  repeat split; try assumption.
-  apply ccw_cyclicity. assumption.
+  repeat split; try assumption;
+  try (apply ccw_cyclicity; assumption).
+Qed.
+
+Theorem graham_convex_0 : forall (p q r : point),
+  sort p [r ; q ; p] -> convex p (convex_hull' [r ; q ; p]).
+Proof.
+  simpl; intros.
+  destruct H as [[H _] [_ _]].
+  destruct (ccw_dec p q r);
+  try (unfold not, ccw, det in *; nia).
+  repeat split; try eauto.
+  apply ccw_cyclicity; assumption.
 Qed.
 
 (** Proof: *)
-(* After the i’th iteration,
-the vertices on T are the vertices of Ci
+(*  After the i’th iteration,
+the vertices on the stack are the vertices of C_i
 in clockwise order.  *)
+
+(*  Prove several properties of [convex_hull']. *)
+(*? partial_ord *)
+Lemma ch'_partial_ord : forall (T : list point),
+  True.
+
+Theorem rightward_conv : forall (p q : point) (T : list point),
+  rightward p q T -> rightward p q (convex_hull' T).
+Proof.
+  induction T; simpl; intros; try eauto.
+  destruct H. specialize (IHT H0).
+  Abort.
+  
+
+Theorem convex_1 : forall (p q : point) (T : list point),
+  sort p (q :: T) -> convex p T -> convex p (succ q T).
+Proof.
+  intros. destruct H.
+  destruct T; try eauto.
+  destruct T; try eauto.
+  revert p0 p1 H H0 H1.
+  induction T; simpl; intros; destruct (ccw_dec p1 p0 q);
+  try (simpl; eauto);
+  try (destruct H;
+    repeat split; simpl; try assumption;
+    try (apply ccw_cyclicity; assumption)).
+  
+
+  Admitted.
+
+Theorem graham_convex_1 : forall (p : point) (T : list point),
+  sort p T -> convex p (convex_hull' T).
+Proof.
+  intros.
+  induction T; simpl; try eauto.
+  pose proof sort_rec p a T H. specialize (IHT H0).
+  pose proof convex_1 p a (convex_hull' T).
+  apply H1; try assumption. clear H0 H1.
+  simpl in *. Abort.
 
 (*
   sort p P' -> convex p (convex_hull_assist P' [p])
@@ -199,5 +253,6 @@ Example pb := {| x := 5; y := 3|}.
 Example pc := {| x := 6; y := 7|}.
 Example pd := {| x := 2; y := 3|}.
 
-Compute convex_hull [pa ; pb ; pc].
-Compute convex_hull [pa ; pb ; pc ; pd].
+Compute convex_hull' [pa ; pb ; pc].
+Compute convex_hull' [pa ; pb ; pc ; pd].
+Check convex pa [pa ; pb ; pc ; pd].
